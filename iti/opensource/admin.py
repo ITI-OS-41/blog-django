44f554f2e4eacc782fcb3727e94bfbb65daab82e
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-21 16:18:27 +0200

    custom filter title

--- iti/opensource/admin.py
+++ iti/opensource/admin.py
@@ -2,13 +2,24 @@ from django.contrib import admin
 from .models import Post, Category
 
 
+def custom_titled_filter(title):
+    class Wrapper(admin.FieldListFilter):
+        def __new__(cls, *args, **kwargs):
+            instance = admin.FieldListFilter.create(*args, **kwargs)
+            instance.title = title
+            return instance
+    return Wrapper
+
+
 class CustomPost(admin.ModelAdmin):
     fieldsets = (
-        ['Post Information', {'fields': ['title','author','status']}],
+        ['Post Information', {'fields': ['title','slug','author','content','status']}],
         ['Category Information', {'fields': ['category']}]
     )
     list_display = ('title', 'category', 'author', 'is_published')
-    list_filter = ['category', 'title']
+    list_filter = (
+        ('category__title', custom_titled_filter('category')),
+        'status')
     search_fields = ['title', 'Category__title']
 
 class InlinePost(admin.StackedInline):
@@ -22,4 +33,7 @@ class CustomCategory(admin.ModelAdmin):
 
 admin.site.register(Post, CustomPost)
 admin.site.register(Category, CustomCategory)
+
+
+
 
