127f8887ece24042fed94e6a63edc074977bb0d2
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-22 18:55:20 +0200

    pagination fix

--- iti/opensource/urls.py
+++ iti/opensource/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('', views.getAllPosts, name='posts'),
     path('post/<int:pk>', views.getPost, name='post'),
     path('post/create/', views.newPost),
-    path('post/edit/<postId>', views.editPost),
-    path('post/delete/<postId>', views.deletePost),
+    path('post/edit/<int:pk>', views.editPost),
+    path('post/delete/<int:pk>', views.deletePost),
     path('like/<int:pk>', views.likePost, name='like_post'),
     path('subscribe/<int:pk>', views.subscribeCategory, name='subscribe_category'),
+    # path('category/<int:pk>', views.category, name='category'),
+    # path('tag/<int:pk>', views.tag, name='tag'),
 ]   
